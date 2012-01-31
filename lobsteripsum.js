26d9df80b040f1bc0bfa48460e11042edd30f693
Erik Lundin <mjor.himself@gmail.com>
2012-01-31 22:35:11 +0100

    Enforce 80 character width limit

--- lobsteripsum.js
+++ lobsteripsum.js
@@ -15,24 +15,27 @@
 
   // the actual word list
   var vocabulary = [
-    'a', 'ac', 'accumsan', 'adipiscing', 'aenean', 'aliquam', 'aliquet', 'amet', 'ante',
-    'arcu', 'at', 'auctor', 'augue', 'bibendum', 'blandit', 'commodo', 'condimentum',
-    'congue', 'consectetur', 'consequat', 'convallis', 'cras', 'cubilia', 'curabitur',
-    'curae', 'cursus', 'dapibus', 'diam', 'dictum', 'dignissim', 'dolor', 'donec', 'dui',
-    'duis', 'e', 'egestas', 'eget', 'eleifend', 'elementum', 'elit',  'enim', 'eros',
-    'est', 'et', 'etiam', 'eu', 'euismod', 'facilisis', 'fames', 'faucibus', 'felis',
-    'fermentum', 'feugiat', 'fringilla', 'lacus', 'laoreet', 'lectus', 'leo', 'libero',
-    'ligula', 'lobortis', 'lorem', 'luctus', 'maecenas', 'magna', 'malesuada', 'massa',
-    'mattis', 'mauris', 'metus', 'mi', 'molestie', 'mollis', 'morbi', 'nam', 'nec',
-    'neque', 'netus', 'nibh', 'nisi', 'nisl', 'non', 'nulla', 'nullam', 'nunc', 'odio',
-    'orci', 'ornare', 'pellentest', 'pharetra', 'phasellus', 'placerat', 'porta',
-    'porttitor', 'posuere', 'potenti', 'praesent', 'pretium', 'primis', 'proin',
-    'pulvinar', 'purus', 'quam', 'quis', 'quisque', 'rhoncus', 'risus', 'rutrum',
-    'sagittis', 'sapien', 'scelerisque', 'sed', 'sem', 'semper', 'senectus', 'sit',
-    'sodales', 'suscipit', 'suspendisse', 'tellus', 'tempor', 'tempus', 'tincidunt',
-    'tortor', 'tristique', 'truculente', 'turpis', 'ullamcorper', 'ultrices',
-    'ultricies', 'universita', 'urna', 'ut', 'varius', 'vehicula', 'vel', 'velit',
-    'venenatis', 'vestibulum', 'vitae', 'vivamus', 'viverra', 'volutpat', 'vulputate'
+    'a', 'ac', 'accumsan', 'adipiscing', 'aenean', 'aliquam', 'aliquet',
+    'amet', 'ante', 'arcu', 'at', 'auctor', 'augue', 'bibendum', 'blandit',
+    'commodo', 'condimentum', 'congue', 'consectetur', 'consequat',
+    'convallis', 'cras', 'cubilia', 'curabitur', 'curae', 'cursus', 'dapibus',
+    'diam', 'dictum', 'dignissim', 'dolor', 'donec', 'dui', 'duis', 'e',
+    'egestas', 'eget', 'eleifend', 'elementum', 'elit',  'enim', 'eros', 'est',
+    'et', 'etiam', 'eu', 'euismod', 'facilisis', 'fames', 'faucibus', 'felis',
+    'fermentum', 'feugiat', 'fringilla', 'lacus', 'laoreet', 'lectus', 'leo',
+    'libero', 'ligula', 'lobortis', 'lorem', 'luctus', 'maecenas', 'magna',
+    'malesuada', 'massa', 'mattis', 'mauris', 'metus', 'mi', 'molestie',
+    'mollis', 'morbi', 'nam', 'nec', 'neque', 'netus', 'nibh', 'nisi', 'nisl',
+    'non', 'nulla', 'nullam', 'nunc', 'odio', 'orci', 'ornare', 'pellentest',
+    'pharetra', 'phasellus', 'placerat', 'porta', 'porttitor', 'posuere',
+    'potenti', 'praesent', 'pretium', 'primis', 'proin', 'pulvinar', 'purus',
+    'quam', 'quis', 'quisque', 'rhoncus', 'risus', 'rutrum', 'sagittis',
+    'sapien', 'scelerisque', 'sed', 'sem', 'semper', 'senectus', 'sit',
+    'sodales', 'suscipit', 'suspendisse', 'tellus', 'tempor', 'tempus',
+    'tincidunt', 'tortor', 'tristique', 'truculente', 'turpis', 'ullamcorper',
+    'ultrices', 'ultricies', 'universita', 'urna', 'ut', 'varius', 'vehicula',
+    'vel', 'velit', 'venenatis', 'vestibulum', 'vitae', 'vivamus', 'viverra',
+    'volutpat', 'vulputate'
   ];
 
   // the shortest and longest words in the vocabulary -- note that there
@@ -173,8 +176,8 @@
             str = lobsteripsum(min, max);
           }
           catch (e) {
-            // print the error message if something went wrong (most likely due to
-            // invalid parameters)
+            // print the error message if something went wrong (most likely due
+            // to invalid parameters)
             str = e.message;
           }
 
